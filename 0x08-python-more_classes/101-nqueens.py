98cac20b000d7d6533192903334c4a1d4c638d0e
NomsuBen <olaniranbenjamin@gmail.com>
2023-01-02 13:47:25 -0800

    Just made a 101-nqueens.py
    file

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,14 +1,10 @@
-#!/usr/bin/pytho3
+#!/usr/bin/python3
 """Solves the N-queens puzzle.
-
 Determines all possible solutions to placing N
 N non-attacking queens on an NxN chessboard.
 Example:
-
     $ ./101-nqueens.py N
-
 N must be an integer greater than or equal to 4.
-
 Attributes:
     board (list): A list of lists representing the chessboard.
     solutions (list): A list of lists containing solutions.
